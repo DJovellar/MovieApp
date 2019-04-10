61f761cf94c05fa9fa73404b6f03681e303bcb3f
DJovellar <davidjovellar93@gmail.com>
2019-04-10 18:01:31 +0200

    Append models.py

--- myMovieApp/models.py
+++ myMovieApp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Movie(models.Model):
+    name = models.CharField(max_length=50)
+    release = models.IntegerField()
+    genre = models.CharField(max_length=50)
+    description = models.CharField(max_length=1000)
+    cast = models.CharField(max_length=1000)
+
+class Review(models.Model):
+    movie = ForeignKey(Movie, on_delete=models.CASCADE)
+    user = ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.IntegerRangeField(null = False, min_value=1, max_value=10)
+    description = models.CharField(max_length=4000)
